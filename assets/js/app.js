a6a6ac761ac5f4053ae6d0865b6b8cc8c73371b4
AlaaAlhilly <alaa.naji.us@gmail.com>
2018-11-15 22:44:45 -0600

    upload the ver html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,55 @@
-function init(){
-    gapi.client.setApiKey("AIzaSyAh9sn6135qdZ9ZxGV41J-_-bWC5tbGy5U");
-    gapi.client.load("youtube","v3",function(){
-        isLoad = true;
-    });
-}
+
 function arrayOfEl(e,t){
     res=e;
     for(var n=0;n<t.length;n++){
       res=res.replace(/\{\{(.*?)\}\}/g,function(e,r){
-        return t[n][r]
+        //   alert(t[n][r]);
+        return t[n][r];
       })
     }
       return res
     }
 function displayVideo(){
     var display = `<div class="videoContainer">
-    <h2>{{video}<h2>
+    <h2>{{video}}<h2>
     <iframe class="rs view" width="640" height="360" src="//www.youtube.com/embed/{{videoId}}" frameborder="0" allowfullscreen>
     </iframe></div>`;
     return display;
 
 }
 
-$(function(){
-    init();
-    $('.addgiffs').click(function(e){
-        e.preventDefault();
-        var q = encodeURIComponent($('#targetVideo').val()).replace(/s/g,'+');
-        var numberOfVids = 3;
-        var videoReq = gapi.client.youtube.search.list({
-            part: 'snippet',
-            type: 'video',
-            q: q,
-            maxResults: numberOfVids,
-            order: 'viewCount',
-            publishedAfter: '2015-01-01T00:00:00Z'
-        });
-        videoReq.execute(function(snap){
-            var videoResult = snap.result;
-            $('#targetVideo').html('');
-            $.each(videoResult.items,function(index,item){
-                $("#imgs").append(arrayOfEl(displayVideo(), [{"video":item.snippet.title, "videoId":item.id.videoId}]));
-            });
-            resetHeight();
-        });
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
+// Called automatically when YouTube API interface is loaded (see line 9).
+function onYouTubeApiLoad() {
+    gapi.client.setApiKey('AIzaSyAh9sn6135qdZ9ZxGV41J-_-bWC5tbGy5U');
+}
+ 
+// Called when the search button is clicked in the html code
+$('.addVids').click(function() {
+    var query = encodeURIComponent($("#targetVideo").val()).replace(/%20/g, "+");
+    // Use the JavaScript client library to create a search.list() API call.
+    var request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        q:query,
+        type: 'video',
+        maxResults: 3,
+        order: 'viewCount',
+        publishedAfter: '2015-01-01T00:00:00Z'
     });
-    $(window).on('resize',resetHeight);
+    request.execute(onSearchResponse);
 });
+function onSearchResponse(response) {
+
+    var videoResult = response.items;
+    $('#targetVideo').html('');
+    $.each(videoResult,function(index,item){
+        $(".imgs").append(arrayOfEl(displayVideo(), [{"video":item.snippet.title, "videoId":item.id.videoId}]));
+    });
+    resetHeight();
+    $(window).on('resize',resetHeight);
+}
 function resetHeight(){
 $('.rs').css('height',$('#targetVideo').width() * 9/16);
 }
